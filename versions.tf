f5f1b12480a0246069dd1caf5d6c695e9f57032f
Nia Ngosi <nia.ngosi@capgemini.com>
2026-01-16 12:10:40 +0000

    CCL-6003 - Setting version to resolve tflint warnings

--- versions.tf
+++ versions.tf
@@ -4,6 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.88.0"
     }
+    random = { source = "hashicorp/random", version = "~> 3.6" }
   }
   required_version = ">= 1.5.0"
 }
