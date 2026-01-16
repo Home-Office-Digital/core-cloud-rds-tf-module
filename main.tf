b9493e364ed2d8144584db2ac9c2363e3c240a9a
Nia Ngosi <nia.ngosi@capgemini.com>
2026-01-16 10:48:05 +0000

    CCL-6003 - Adjusting vars to resolve tflint warnings

--- main.tf
+++ main.tf
@@ -68,31 +68,31 @@ resource "aws_security_group" "this" {
 resource "aws_db_instance" "this" {
   for_each = var.instances
 
-  allocated_storage          = lookup(each.value, "snapshot_identifier", null) == null ? each.value.allocated_storage : null
-  availability_zone          = var.availability_zone
-  auto_minor_version_upgrade = var.auto_minor_version_upgrade
-  backup_retention_period    = var.backup_retention_period
-  backup_window              = var.backup_window
-  ca_cert_identifier         = var.ca_cert_identifier
-  db_name                    = lookup(each.value, "snapshot_identifier", null) == null ? each.value.database_name : null
-  db_subnet_group_name       = var.db_subnet_group_name != null ? data.aws_db_subnet_group.existing[0].name : aws_db_subnet_group.this[0].name
-  deletion_protection        = var.deletion_protection
-  engine                     = lookup(each.value, "snapshot_identifier", null) == null ? each.value.engine : null
-  engine_version             = lookup(each.value, "snapshot_identifier", null) == null ? each.value.engine_version : null
-  final_snapshot_identifier  = lookup(each.value, "final_snapshot_identifier", null)
-  identifier                 = each.value.name
-  instance_class             = each.value.instance_class
-  kms_key_id                 = var.kms_key_arn != null ? var.kms_key_arn : null
-  maintenance_window         = each.value.maintenance_window
-  multi_az                   = var.multi_az
-  password                   = aws_secretsmanager_secret_version.rds_password[each.key].secret_string
+  allocated_storage            = lookup(each.value, "snapshot_identifier", null) == null ? each.value.allocated_storage : null
+  availability_zone            = var.availability_zone
+  auto_minor_version_upgrade   = var.auto_minor_version_upgrade
+  backup_retention_period      = var.backup_retention_period
+  backup_window                = var.backup_window
+  ca_cert_identifier           = var.ca_cert_identifier
+  db_name                      = lookup(each.value, "snapshot_identifier", null) == null ? each.value.database_name : null
+  db_subnet_group_name         = var.db_subnet_group_name != null ? data.aws_db_subnet_group.existing[0].name : aws_db_subnet_group.this[0].name
+  deletion_protection          = var.deletion_protection
+  engine                       = lookup(each.value, "snapshot_identifier", null) == null ? each.value.engine : null
+  engine_version               = lookup(each.value, "snapshot_identifier", null) == null ? each.value.engine_version : null
+  final_snapshot_identifier    = lookup(each.value, "final_snapshot_identifier", null)
+  identifier                   = each.value.name
+  instance_class               = each.value.instance_class
+  kms_key_id                   = var.kms_key_arn != null ? var.kms_key_arn : null
+  maintenance_window           = each.value.maintenance_window
+  multi_az                     = var.multi_az
+  password                     = aws_secretsmanager_secret_version.rds_password[each.key].secret_string
   performance_insights_enabled = var.performance_insights_enabled
-  publicly_accessible = var.publicly_accessible
-  snapshot_identifier        = var.snapshot_identifier
-  storage_type               = var.storage_type
-  storage_encrypted          = var.storage_encrypted
-  skip_final_snapshot        = var.skip_final_snapshot
-  username                   = lookup(each.value, "snapshot_identifier", null) == null ? var.database_user : null
+  publicly_accessible          = var.publicly_accessible
+  snapshot_identifier          = var.snapshot_identifier
+  storage_type                 = var.storage_type
+  storage_encrypted            = var.storage_encrypted
+  skip_final_snapshot          = var.skip_final_snapshot
+  username                     = lookup(each.value, "snapshot_identifier", null) == null ? var.database_user : null
   vpc_security_group_ids = concat(
     [
       var.security_group_ids != null ?
