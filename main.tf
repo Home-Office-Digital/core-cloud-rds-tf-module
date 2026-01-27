507e1a1c5b5a41647c0d45a1026437b0f0a37e31
Nia Ngosi <nia.ngosi@capgemini.com>
2026-01-27 10:08:03 +0000

    CCL-6003 - Adding data block for existing SG Group

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "aws_db_instance" "this" {
   storage_encrypted               = var.storage_encrypted
   skip_final_snapshot             = var.skip_final_snapshot
   username                        = lookup(each.value, "snapshot_identifier", null) == null ? var.database_user : null
-  vpc_security_group_ids          = var.security_group_ids != null ? concat([var.security_group_ids[each.key]], var.vpc_security_group_ids) : concat([aws_security_group.this[each.key].id], var.vpc_security_group_ids)
+  vpc_security_group_ids          = var.security_group_ids != null ? concat([var.security_group_ids[each.key]], var.vpc_security_group_ids) : concat([var.security_group_ids != null ? data.aws_security_group.existing[each.key].id : aws_security_group.this[each.key].id], var.vpc_security_group_ids)
 
   tags = merge(var.tags, {
     Name = each.value.name
